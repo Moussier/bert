5f3613c6afb77177fa5e84833c2260cdd8ca59fa
Moussier <arthur.bensimon@gmail.com>
2019-01-16 09:52:49 +0100

    weird bug while predicting, keyerror

--- run_classifier.py
+++ run_classifier.py
@@ -541,7 +541,7 @@ def file_based_convert_examples_to_features(
     if ex_index % 10000 == 0:
       tf.logging.info("Writing example %d of %d" % (ex_index, len(examples)))
 
-    feature = convert_single_example(ex_index, example, label_list,
+      feature = convert_single_example(ex_index, example, label_list,
                                      max_seq_length, tokenizer)
 
     def create_int_feature(values):
